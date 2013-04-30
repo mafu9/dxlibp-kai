625faa48f51bde59b987ae6cff5f35c646271567
mafu9 <mafu9mafu@gmail.com>
2013-04-30 10:08:57 +0900

    Add dxpSoundTerm in DxLib_End

--- src/general/dxlib_end.c
+++ src/general/dxlib_end.c
@@ -1,10 +1,12 @@
 #include "../general.h"
+#include "../sound.h"
 #include "../graphics.h"
 #include "../memory.h"
 int DxLib_End()
 {
 	if(!dxpGeneralData.initialized)return 0;
 //	InitSoundMem();
+	dxpSoundTerm();
 	dxpGraphicsEnd();
 	dxpMemoryEnd();
 	return 0;
