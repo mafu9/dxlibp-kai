4bbf93cc98a1c3e9a9c0ced337b87cde44d992a9
mafu <mafu9mafu@gmail.com>
2013-05-26 08:05:53 +0900

    Enable to allocate memory from global value
    Here is an example:
    static std::string s("aaaa");

--- src/memory/memutils.c
+++ src/memory/memutils.c
@@ -55,62 +55,62 @@ static void dxpMemoryError(void)
 void* __wrap_malloc(size_t size)
 {
 	void *ptr;
-	LockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)LockMutex(dxpMemoryData.mutexHandle);
 	ptr = __real_malloc(size);
 	if(ptr == NULL && size != 0 && dxpMemoryIsSingleLocked() && dxpMemoryData.memAssert)
 	{
 		fprintf(stderr, "malloc failed: %ubytes\n", size);
 		dxpMemoryError();
 	}
-	UnlockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)UnlockMutex(dxpMemoryData.mutexHandle);
 	return ptr;
 }
 
 void* __wrap_realloc(void *ptr, size_t size)
 {
 	void *newPtr;
-	LockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)LockMutex(dxpMemoryData.mutexHandle);
 	newPtr = __real_realloc(ptr, size);
 	if(newPtr == NULL && size != 0 && dxpMemoryIsSingleLocked() && dxpMemoryData.memAssert)
 	{
 		fprintf(stderr, "realloc failed: %p %ubytes\n", ptr, size);
 		dxpMemoryError();
 	}
-	UnlockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)UnlockMutex(dxpMemoryData.mutexHandle);
 	return newPtr;
 }
 
 void *__wrap_calloc(size_t n, size_t size)
 {
 	void *ptr;
-	LockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)LockMutex(dxpMemoryData.mutexHandle);
 	ptr = __real_calloc(n, size);
 	if(ptr == NULL && n != 0 && size != 0 && dxpMemoryIsSingleLocked() && dxpMemoryData.memAssert)
 	{
 		fprintf(stderr, "calloc failed: %ublocks %ubytes\n", n, size);
 		dxpMemoryError();
 	}
-	UnlockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)UnlockMutex(dxpMemoryData.mutexHandle);
 	return ptr;
 }
 
 void* __wrap_memalign(size_t align, size_t size)
 {
 	void *ptr;
-	LockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)LockMutex(dxpMemoryData.mutexHandle);
 	ptr = __real_memalign(align, size);
 	if(ptr == NULL && size > 0 && dxpMemoryIsSingleLocked() && dxpMemoryData.memAssert)
 	{
 		fprintf(stderr, "memalign failed: %ubytes(aligned %ubytes)\n", size, align);
 		dxpMemoryError();
 	}
-	UnlockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)UnlockMutex(dxpMemoryData.mutexHandle);
 	return ptr;
 }
 
 void __wrap_free(void *ptr)
 {
-	LockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)LockMutex(dxpMemoryData.mutexHandle);
 	__real_free(ptr);
-	UnlockMutex(dxpMemoryData.mutexHandle);
+	if(dxpMemoryData.mutexHandle != -1)UnlockMutex(dxpMemoryData.mutexHandle);
 }
