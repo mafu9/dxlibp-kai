3011991319d570fe27e4c1ccf449323962688577
mafu <mafu9mafu@gmail.com>
2013-06-11 00:43:50 +0900

    Fix buffer overrun

--- src/fileio/fileread_gets.c
+++ src/fileio/fileread_gets.c
@@ -4,7 +4,7 @@ int FileRead_gets(char *buffer,int buffersize,int filehandle)
 {
 	int i,c;
 	if(!dxpFileioData.init)return -1;
-	for(i = 0;i < buffersize;++i)
+	for(i = 0;i < buffersize - 1;++i)
 	{
 		c = FileRead_getc(filehandle);
 		if(c == '\0' || c == '\n' || c == -1)break;
